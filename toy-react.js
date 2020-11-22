0e791802872933e9faf3bd3ab38fcf22254e0771
zhangqiuyu <qiuyuzhqy@163.com>
2020-11-22 17:55:33 +0800

    用range把DOM绘制上去

--- toy-react.js
+++ toy-react.js
@@ -1,3 +1,5 @@
+const RENDER_TO_DOM = Symbol("render to dom");
+
 class ElementWrapper {
     constructor(type) {
         this.root = document.createElement(type);
@@ -6,13 +8,24 @@ class ElementWrapper {
         this.root.setAttribute(name, value);
     }
     appendChild(component) {
-        this.root.appendChild(component.root);
+        let range = document.createRange();
+        range.setStart(this.root, this.root.childNodes.length);
+        range.setEnd(this.root, this.root.childNodes.length);
+        component[RENDER_TO_DOM](range);
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 }
 
 class TextWrapper {
     constructor(content) {
         this.root = document.createTextNode(content);
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 }
 
@@ -30,17 +43,22 @@ export class Component {
     }
     // 不再用root进行操作
     // 思想上的变化：从取一个元素，到把它渲染进一个range里面
-    _renderToDOM(range) {
-        this.render()._renderToDOM(range);
+
+    // 写法讲解：这里如果是用字符串的话，不管是class里面，还是object里面，
+    // 都可以用方括号的形式，来代替这个名字，表示它里面是一个变量
+    // 相当于把 Symbo("render to dom")放到了这里
+    // 这种写法可以让它不太容易被访问到
+    [RENDER_TO_DOM](range) {
+        this.render()[RENDER_TO_DOM](range);
     }
     // 如果render回来的结构，是一个Component的子类，
     // 那么它就会对root进行一个递归的调用
-    get root() {
-        if(!this._root) {
-            this._root = this.render().root;
-        }
-        return this._root;
-    }
+    // get root() {
+    //     if(!this._root) {
+    //         this._root = this.render().root;
+    //     }
+    //     return this._root;
+    // }
 }
 
 export function createElement(type, attributes, ...children) {
@@ -73,5 +91,9 @@ export function createElement(type, attributes, ...children) {
 }
 
 export function render(component, parentElement) {
-    parentElement.appendChild(component.root);
+    let range = document.createRange();
+    range.setStart(parentElement, 0);
+    range.setEnd(parentElement, parentElement.childNodes.length);
+    range.deleteContents();
+    component[RENDER_TO_DOM](range);
 }
